ccf8f0c3faa131e9b67bf880b717c15f09e29758
Josh Winters <jwinters@pivotal.io>
2019-05-11 10:50:17 -0400

    atc: add checker
    Signed-off-by: Joshua Winters <jwinters@pivotal.io>
    Co-authored-by: Rui Yang <ryang@pivotal.io>

--- atc/db/resource_factory.go
+++ atc/db/resource_factory.go
@@ -1,8 +1,6 @@
 package db
 
 import (
-	"errors"
-
 	sq "github.com/Masterminds/squirrel"
 	"github.com/concourse/concourse/atc/db/lock"
 )
@@ -11,7 +9,6 @@ import (
 
 type ResourceFactory interface {
 	VisibleResources([]string) ([]Resource, error)
-	ResourceChecks() ([]ResourceCheck, error)
 }
 
 type resourceFactory struct {
@@ -56,8 +53,4 @@ func (r *resourceFactory) VisibleResources(teamNames []string) ([]Resource, erro
 	}
 
 	return resources, nil
-}
-
-func (r *resourceFactory) ResourceChecks() ([]ResourceCheck, error) {
-	return nil, errors.New("nope")
 }
