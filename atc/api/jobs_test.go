271de293ce21f7133b93974aedb58b38f80b3213
Joshua Winters <jwinters@pivotal.io>
2019-08-01 12:01:20 -0400

    atc: run check from pinned version for trigger job
    Signed-off-by: Joshua Winters <jwinters@pivotal.io>
    Co-authored-by: Rui Yang <ryang@pivotal.io>

--- atc/api/jobs_test.go
+++ atc/api/jobs_test.go
@@ -1517,6 +1517,7 @@ var _ = Describe("Jobs API", func() {
 							BeforeEach(func() {
 								fakeResource = new(dbfakes.FakeResource)
 								fakeResource.NameReturns("some-input")
+								fakeResource.CurrentPinnedVersionReturns(atc.Version{"some": "version"})
 
 								fakePipeline.ResourcesReturns([]db.Resource{fakeResource}, nil)
 							})
@@ -1551,6 +1552,11 @@ var _ = Describe("Jobs API", func() {
 
 								It("creates a check for the resource", func() {
 									Expect(dbCheckFactory.TryCreateCheckCallCount()).To(Equal(1))
+								})
+
+								It("runs the check from the current pinned version", func() {
+									_, _, fromVersion, _ := dbCheckFactory.TryCreateCheckArgsForCall(0)
+									Expect(fromVersion).To(Equal(atc.Version{"some": "version"}))
 								})
 
 								It("notifies the checker to run", func() {
